9f61540bfef1c761fcd4a9b6a87f5a2ed58c7c7f
root <root@DESKTOP-N10Q070.localdomain>
2020-02-11 13:42:47 -0600

    stuff

--- db/db.js
+++ db/db.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql2");
-
-let connection = mysql.createConnection({
+let conn = {
   port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
-});
+}
+ if (process.env.JAWSDB_URL){
+   conn = process.env.JAWSDB_URL
+ }
+  
+
+let connection = mysql.createConnection(conn);
 
 connection.connect(function (err) {
   if (err) throw err;
